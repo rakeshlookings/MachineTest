92e33de00c1a579c6edf6cf17f417a693bbb1f2a
softquakers <81610881+softquakers@users.noreply.github.com>
2022-10-19 01:04:27 +0530

    make it better

--- src/routes/index.router.js
+++ src/routes/index.router.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const router = express.Router()
 const LedgerRouter = require('./Ledger.routes')
-router.get('/',(req,res)=> { res.send('ok')})
 router.use('/ledger', LedgerRouter)
 
 module.exports = router
