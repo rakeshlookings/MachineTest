85d1c6811ceb623810b72d7b3a5d5cbe1be48755
softquakers <81610881+softquakers@users.noreply.github.com>
2022-10-19 01:24:33 +0530

    make it better

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import router from './src/routes/Index.router'
+const router = reuire('./src/routes/Index.router')
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
-const LedgerRouter = require('./Ledger.routes')
+const LedgerRouter = require('./ledger.routes')
 router.use('/ledger', LedgerRouter)
-
+router.get('/sample', (req,res)=> {
+    res.send('ok router')
+})
 module.exports = router
