48376d13c3c4bf46753ab408aa227664722fcb26
softquakers <81610881+softquakers@users.noreply.github.com>
2022-10-19 01:26:03 +0530

    make it better

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const router = reuire('./src/routes/Index.router')
+const router = reuire('./src/routes')
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
