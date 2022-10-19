0f07b8ce9b21f3689a6cbacbae763806b8abdd10
softquakers <81610881+softquakers@users.noreply.github.com>
2022-10-19 10:27:35 +0530

    port change

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.get('/', (req,res)=> {
 
 app.use('/api/v1', router)
 
-const PORT = 8000 // process.env.PORT || 8080
+const PORT = process.env.PORT || 8080
 
 app.listen(PORT, () => console.log('server started'))
 
